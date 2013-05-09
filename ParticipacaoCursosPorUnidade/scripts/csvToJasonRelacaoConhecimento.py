aac6a24ae423d43fa004a1e7be9ae84263984ef3
Gabriel Mello <gabrielmll@gmail.com>
2013-05-08 21:12:19 -0300

    Legenda estável sem highlight

--- ParticipacaoCursosPorUnidade/scripts/csvToJasonRelacaoConhecimento.py
+++ ParticipacaoCursosPorUnidade/scripts/csvToJasonRelacaoConhecimento.py
@@ -6,7 +6,7 @@ import json
 
 f = open( '../csv/relacaoCursoGrandeArea.csv', 'r' )
 
-reader = csv.DictReader( f, fieldnames = ( "CURSO","UNIDADE","AREA", "GRANDE AREA" ) )
+reader = csv.DictReader( f, fieldnames = ( "CURSO","UNIDADE","AREA", "GRANDE AREA", "APELIDO" ) )
 out = json.dumps( [ row for row in reader ] )
 output = json.loads(out)
 
@@ -19,6 +19,7 @@ for i in output:
 	j['unidade'] = str(i['UNIDADE'])
 	j['area'] = str(i['AREA']).encode('latin-1')
 	j['grandearea'] = str(i['GRANDE AREA']).encode('latin-1')
+	j['apelido'] = str(i['APELIDO']).encode('latin-1')
 	
 	cursos.append(j)
 
